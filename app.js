1973664791bacfa1e9231534879b1326ff1199bb
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2021-01-29 17:29:11 -0500

    Update Portfolio Apps
    Add bLogPoster app to projects section

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
 
-const express = require("express");
-const mongoose = require("mongoose");
-const weatherRoute = require("./Routes/weatherRoute");
-const gameRoute = require("./Routes/gameRoute");
-const todoListRoute = require("./Routes/todolistRoute");
-const basketballRoute = require("./Routes/basketballRoute");
+const express = require('express');
+const mongoose = require('mongoose');
+const weatherRoute = require('./Routes/weatherRoute');
+const gameRoute = require('./Routes/gameRoute');
+const todoListRoute = require('./Routes/todolistRoute');
+const basketballRoute = require('./Routes/basketballRoute');
 
 const app = express();
 
@@ -17,35 +17,35 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Connected to MongoDB..."))
+  .then(() => console.log('Connected to MongoDB...'))
   .catch((err) => console.log(err));
 
 // Connect to Database
 app.listen(3000);
 
 // Register View Engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // Middleware & Static Files
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.get("/", (req, res) => {
-  res.redirect("/home");
+app.get('/', (req, res) => {
+  res.redirect('/home');
 });
 
-app.get("/home", (req, res) => {
-  res.render("index");
+app.get('/home', (req, res) => {
+  res.render('index');
 });
 
-app.use("/", gameRoute);
-app.use("/", weatherRoute);
-app.use("/", todoListRoute);
-app.use("/", basketballRoute);
+app.use('/', gameRoute);
+app.use('/', weatherRoute);
+app.use('/', todoListRoute);
+app.use('/', basketballRoute);
 
-app.get("/download", (req, res) => {
+app.get('/download', (req, res) => {
   const file = `${__dirname}/upload/PrabdeepDhaliwalsResume.pdf`;
   res.download(file);
 });
--- models/user.js
+++ models/user.js
@@ -1,23 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    max: 30
+const userSchema = new Schema(
+  {
+    displayName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  comments: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 400
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
